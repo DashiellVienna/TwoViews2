913cfa42265974e02fda01d290e57ff466130a26
Andrei <101584932+DashiellVienna@users.noreply.github.com>
2024-10-14 15:09:54 +0200

    Initial Commit

---
+++ TwoViews2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViews2
+//
+//  Created by Andrei Deshin on 14.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
